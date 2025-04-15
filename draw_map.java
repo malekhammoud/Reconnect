4b8126814053e59af31f432f5e100ae9875eca34
Malek Hammoud <362858318@gotvdsb.ca>
2025-04-15 12:37:34 -0400

    fixed draw map logic

--- draw_map.java
+++ draw_map.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class draw_map extends JFrame {
+public class draw_map extends JFrame  implements MouseListener, MouseMotionListener, KeyListener {
+    int height = 50;
+    int width= 50;
+    int value = 1;
+    /*
     int[][] main_map= { {0,0,0,0,0,0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0},
             {0,0,0,0,1,1,0,0,0,0,0,0,0,0},
@@ -12,13 +17,15 @@ public class draw_map extends JFrame {
             {0,0,0,0,1,1,0,0,0,0,0,0,0,0},
             {0,0,0,0,1,1,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+     */
+    int[][] main_map = new int[height][width];
+    int size = 10;
     void drawmap(Graphics g, int[][] maps){
-        int size = 10;
         for (int y = 0; y < maps.length; y++){
             for (int x = 0; x < maps[y].length; x++){
-                if (main_map[y][x] == 0){
+                if (main_map[y][x] ==1){
                     g.setColor(Color.BLACK);
-                    g.fillRect(x*size, y*size, size,size);
+                    g.fillRect(x*size, y*size-40, size,size);
                 };
             }
         }
@@ -41,7 +48,75 @@ public class draw_map extends JFrame {
         this.add(panel);
         this.pack();
         this.setVisible(true);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        addKeyListener(this);
     }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        main_map[e.getY()/size][e.getX()/size] = value;
+        repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key= e.getKeyCode();
+        if (key == KeyEvent.VK_D){
+            value = 0;
+        }
+        if (key == KeyEvent.VK_SPACE){
+            for (int y = 0; y < main_map.length; y++) {
+                for (int x = 0; x < main_map[y].length; x++) {
+                    System.out.print(main_map[y][x] + " ");
+                }
+                System.out.println();
+            }
+        }
+        else{
+            value = 1;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+
     private class DrawingPanel extends JPanel {
         DrawingPanel(){
             this.setPreferredSize(new Dimension(500,500));
