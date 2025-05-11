b9b1007cd6ada45335e1f73035a1f9f6d3d0d572
Malek Hammoud <362858318@gotvdsb.ca>
2025-05-11 19:21:51 -0400

    Smoother animation this way

--- src/playerMotion.java
+++ src/playerMotion.java
@@ -82,7 +82,7 @@ public class playerMotion extends JFrame implements KeyListener, MouseMotionList
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             Graphics2D g2 = (Graphics2D)g;
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            //g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             mainMap.draw(g);
             player.draw(g);
             g.setColor(Color.WHITE);
